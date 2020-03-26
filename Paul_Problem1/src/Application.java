d244561eaeb5346a72784bd72f1ff45dd4c95f85
Karam Paul <karampaul@me.com>
2020-03-26 16:20:20 -0400

    Working Solution

--- Paul_Problem1/src/Application.java
+++ Paul_Problem1/src/Application.java
@@ -1,2 +1,28 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.NoSuchElementException;
+
 public class Application {
+    public static void main(String[] args) {
+        DuplicateRemover duplicateRemover = new DuplicateRemover();
+
+        try {
+            duplicateRemover.remove("problem1.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Please double check your filename\n");
+        } catch (NoSuchElementException e) {
+            System.out.println("NoSuchElementException has been triggered, but program still continued successfully.\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Something is wrong\n");
+        }
+
+        try {
+            duplicateRemover.write("unique_words.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
--- Paul_Problem1/src/DuplicateRemover.java
+++ Paul_Problem1/src/DuplicateRemover.java
@@ -1,2 +1,53 @@
+import java.io.*;
+import java.util.Scanner ;
+import java.util.HashSet ;
+
+
 public class DuplicateRemover {
+    //Instance Variables
+    private HashSet<String> uniqueWords = new HashSet<String>() ;//HashSets seemed to be the best approach
+
+
+    // Method to accept a file input and take out duplicate words
+    public void remove(String dataFile) throws Exception {
+        File file = new File(dataFile);
+        Scanner input = new Scanner(file);
+
+        while (input.hasNextLine()) {
+            String next = input.next();
+            System.out.println(next);
+            next = next.toLowerCase();      //makes string lower case
+            next = next.replaceAll("\\p{Punct}", "");  //removes punctuation from the String
+            //System.out.println(next);     Test Code
+            uniqueWords.add(next);
+        }
+        //Test Code
+        //System.out.println(uniqueWords);
+        //System.out.println(uniqueWords.size());
+    }
+
+    public void write(String outputFile) throws IOException {
+        File file = null;
+        FileWriter fileWriter = null;
+
+        file = new File(outputFile);
+
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        fileWriter = new FileWriter(file);
+        fileWriter.write(String.valueOf(uniqueWords));
+        fileWriter.close();
+        System.out.print("File Writing is Complete! Check out the text file!");
+
+    }
+
+//        Writer out = new FileWriter(outputFile);
+//        out.write(String.valueOf(uniqueWords));
+//        out.flush();
+//        out.close();
+
+        //Test code
 }
+
